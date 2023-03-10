b7356dd29420a5df490a585ada8094f059c0c665
Omar <annexomar@gmail.com>
2023-03-10 20:16:18 +0300

    updated permissions

--- Routes/ProductRoute.js
+++ Routes/ProductRoute.js
@@ -2,18 +2,18 @@ const router = require("express").Router();
 const Product = require("../Models/ProductModel");
 const { verifyToken, verifyAdmin } = require("../Utils/VerifyJwt");
 
-router.post("/create", async (req, res) => {
+router.post("/create", verifyToken, async (req, res) => {
   const product = req.body;
   const newProduct = await Product.create(product);
   res.status(201).send(newProduct);
 });
 
-router.get("/find/:id", verifyAdmin, async (req, res) => {
+router.get("/find/:id", verifyToken, async (req, res) => {
   const product = await Product.findById(req.params.id);
   res.status(200).json(product);
 });
 
-router.put("/update/:id", verifyAdmin, async (req, res) => {
+router.put("/update/:id", verifyToken, async (req, res) => {
   const product = await Product.findByIdAndUpdate(
     req.params.id,
     {
@@ -24,7 +24,7 @@ router.put("/update/:id", verifyAdmin, async (req, res) => {
   res.status(200).send(product);
 });
 
-router.delete("/delete/:id", async (req, res) => {
+router.delete("/delete/:id", verifyToken, async (req, res) => {
   const product = await Product.findByIdAndDelete(req.params.id);
   res.send("Product Deleted");
 });
